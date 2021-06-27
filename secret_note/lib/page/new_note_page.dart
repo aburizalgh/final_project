3b7d1d8b64e667f6d8a947fbd9e22fe3d70c7193
aburizalgh <86548055+aburizalgh@users.noreply.github.com>
2021-06-27 20:54:41 +0700

    Update new_note_page.dart

--- secret_note/lib/page/new_note_page.dart
+++ secret_note/lib/page/new_note_page.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class NewNote extends StatelessWidget {
+class NewNote extends StatefulWidget {
+  List list;
+  int index;
+
+  NewNote({this.index, this.list});
+
+  @override
+  _NewNoteState createState() => _NewNoteState();
+}
+
+class _NewNoteState extends State<NewNote> {
+  TextEditingController controllerTitle;
+  TextEditingController controllerCategory;
+  TextEditingController controllerContent;
+
+  void addData() {
+    var url = "http://localhost/secret_note/adddata.php";
+
+    http.post(url, body: {
+      "title": controllerTitle.text,
+      "category": controllerCategory.text,
+      "content": controllerContent.text,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("New Note"),
       ),
@@ -23,7 +49,21 @@ class NewNote extends StatelessWidget {
                     border: InputBorder.none,
                     hintText: "Title",
                     hintStyle: TextStyle(color: Colors.black38)),
-                controller: TextEditingController(),
+                controller: controllerTitle,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border(
+                      bottom: BorderSide(color: Colors.black12, width: 5))),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Category",
+                    hintStyle: TextStyle(color: Colors.black38)),
+                controller: controllerCategory,
               ),
             ),
             Container(
@@ -36,15 +76,17 @@ class NewNote extends StatelessWidget {
                     border: InputBorder.none,
                     hintText: "Content",
                     hintStyle: TextStyle(color: Colors.black38)),
-                controller: TextEditingController(),
+                controller: controllerContent,
               ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () => Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => NewNote())),
+          onPressed: () {
+            addData();
+            Navigator.pop(context);
+          },
           child: Icon(Icons.check)),
     );
   }
