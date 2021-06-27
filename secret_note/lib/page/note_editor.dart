af0bf74c321aeb10e1f5c31bb73bc2476c227f4e
aburizalgh <86548055+aburizalgh@users.noreply.github.com>
2021-06-27 20:55:21 +0700

    Update note_editor.dart

--- secret_note/lib/page/note_editor.dart
+++ secret_note/lib/page/note_editor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class noteEditor extends StatefulWidget {
   List list;
@@ -11,11 +12,54 @@ class noteEditor extends StatefulWidget {
 }
 
 class _noteEditorState extends State<noteEditor> {
+  TextEditingController controllerTitle;
+  TextEditingController controllerCategory;
+  TextEditingController controllerContent;
+
+  void editData() {
+    var url = "http://localhost/secret_note/updatedata.php";
+    http.post(url, body: {
+      "id": widget.list[widget.index]['title'],
+      "title": controllerTitle.text,
+      "category": controllerCategory.text,
+      "content": controllerContent.text,
+    });
+  }
+
+  void delData() {
+    var url = "http://localhost/secret_note/deldata.php";
+    http.post(url, body: {
+      "title": widget.list[widget.index]['title'],
+    });
+  }
+
+  void initState() {
+    controllerTitle =
+        new TextEditingController(text: widget.list[widget.index]['title']);
+    controllerCategory =
+        new TextEditingController(text: widget.list[widget.index]['category']);
+    controllerContent =
+        new TextEditingController(text: widget.list[widget.index]['content']);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text("Read Note"),
+        actions: [
+          TextButton(
+              onPressed: () {
+                delData();
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Delete",
+                style: TextStyle(color: Colors.white),
+              ))
+        ],
       ),
       body: Container(
         height: double.infinity,
@@ -29,26 +73,39 @@ class _noteEditorState extends State<noteEditor> {
                       bottom: BorderSide(color: Colors.black12, width: 5))),
               padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
               child: TextField(
-                readOnly: true,
                 decoration: InputDecoration(border: InputBorder.none),
-                controller: TextEditingController(
-                    text: widget.list[widget.index]['title']),
+                controller: controllerTitle,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border(
+                      bottom: BorderSide(color: Colors.black12, width: 5))),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: TextField(
+                decoration: InputDecoration(border: InputBorder.none),
+                controller: controllerCategory,
               ),
             ),
             Container(
               padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
               color: Colors.white,
               child: TextField(
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                decoration: InputDecoration(border: InputBorder.none),
-                controller: TextEditingController(
-                    text: widget.list[widget.index]['content']),
-              ),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: InputDecoration(border: InputBorder.none),
+                  controller: controllerContent),
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            editData();
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.check)),
     );
   }
 }
